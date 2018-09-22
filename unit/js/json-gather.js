4412f06ff1f7fd3deac2c9eef9a6d757178f2f76
Mitsu <42939404+aoihanii@users.noreply.github.com>
2018-09-22 14:54:55 -0300

    Update json-gather.js

--- unit/js/json-gather.js
+++ unit/js/json-gather.js
@@ -135,7 +135,7 @@
 					if(window.chara[i][auto] != ""){
 							autoskill += '<div class="base-ability">' 
 								  +  	'<div class="description">'
-								  +	'<img src="../common/assets/autoskill/' + window.chara[i][autoimg] + '.png" align="left">'
+								  +	'<img src="../common/assets/autoskill/' + window.chara[i][autoimg] + '.png" align="left" width="80px" style="margin-left:10px">'
 								  +			'<h3 id="ability' + x + '">' + window.chara[i][autoname] + '</h3>'
 								  +			'<p id="ability' + x + '">' + window.chara[i][auto] + '</p>'
 								  +		'</div>'
